7374b593c0b9e2d2c89f1bb8005a171e566caf19
haggau15 <haggau94@gmail.com>
2024-02-24 16:38:36 +0100

    the places can now be sorted from lowest to highest in terms of google maps rating

--- filter.py
+++ filter.py
@@ -6,10 +6,16 @@ app = Flask(__name__)
 CORS(app)
 
 
-# Define a GET endpoint
 @app.route('/filter', methods=['POST'])
-def hello():
+def low_to_high():
     request_body = request.json
+    temp = []
+    for i in range(0, len(request_body)-1):
+        # Score is initally set to a higher number than possible in the google score
+        for n in range(0, len(request_body)-1):
+            if request_body[n]['score'] > request_body[n + 1]['score']:
+                request_body[n], request_body[n + 1] = request_body[n + 1], request_body[n]
+
     return jsonify(request_body)
 
 
